d130fb5f481ca064f99f4f80cddd1dbca3019231
yzxty <69448178+yzxty@users.noreply.github.com>
2020-08-14 10:22:35 +0800

    Update py5_1.py

--- python/py5_1.py
+++ python/py5_1.py
@@ -13,3 +13,11 @@ class Rect:
             return True
         else:
             return False
+
+import unittest
+class RectTest(unittest.TestCase):
+    def test_intersect_for_crossing_rects(self):
+        self.assertTrue(Rect(1, 1, 1, 1).intersect(Rect(1, 1, 0.5, 0.5)))
+
+if __name__ == '__main__':
+    unittest.main()
